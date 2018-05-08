f3ca347f7e0a9e52e98a98c7bc7bf064aa901795
Nicky de Vendt <nicky@sensimedia.nl>
2018-05-08 13:50:32 +0200

    returning to basics one step at a time

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,16 +3,40 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\AbstractController;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-    class HomepageController extends AbstractController
+    class HomepageController extends Controller
     {
         /**
             * @Route("/")
             */
         public function homepage() {
-            return $this->render('index.html.twig', array());
+            $_SESSION['admin'] = 1;
+            $_SESSION['userid'] = 1;
+
+            return $this->render('page.html.twig', array(
+                'session' => $_SESSION, 'companys' => $this->getCompanys(), 'projects' => $this->getProjects(),
+            ));
+        }
+
+        function getCompanys() : array {
+            $stmt = $pdo->prepare('SELECT * FROM affiliatedcompanys ORDER BY datecreated ASC LIMIT 5');
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        function getProjects() : array {
+            $statement = connection()->prepare('SELECT * FROM projects ORDER BY id DESC LIMIT 3');
+            $statement->execute();
+            return $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        function connection() {
+            $pdo = new PDO('pgsql:host=localhost;dbname=nicky;', 'nicky', 'blarps');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
         }
     }
 
