fd366e3565ba0f99b589c2df708e6bb64ac52c2b
Nicky de Vendt <nicky@sensimedia.nl>
2018-05-08 11:04:26 +0200

    all sorts of files

---
+++ src/Homepage/home.php
@@ -0,0 +1,18 @@
+<?php
+$pdo = connection();
+function getCompanys() : array {
+    $stmt = $pdo->prepare('SELECT * FROM affiliatedcompanys ORDER BY datecreated ASC LIMIT 5');
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+function getProjects() : array {
+    $statement = $pdo->prepare('SELECT * FROM projects ORDER BY id DESC LIMIT 3');
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+function connection() {
+    $pdo = new PDO('pgsql:host=localhost;dbname=nicky;', 'nicky', 'blarps');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
+
