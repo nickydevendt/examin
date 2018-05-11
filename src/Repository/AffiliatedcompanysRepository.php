01d1ae323f0b265bb24612d3e6493a222e6670e7
Nicky de Vendt <nicky@sensimedia.nl>
2018-05-11 13:43:07 +0200

    initial databasing

---
+++ src/Repository/AffiliatedcompanysRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Affiliatedcompanys;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Affiliatedcompanys|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Affiliatedcompanys|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Affiliatedcompanys[]    findAll()
+ * @method Affiliatedcompanys[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AffiliatedcompanysRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Affiliatedcompanys::class);
+    }
+
+//    /**
+//     * @return Affiliatedcompanys[] Returns an array of Affiliatedcompanys objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Affiliatedcompanys
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
