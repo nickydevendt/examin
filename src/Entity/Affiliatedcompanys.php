dc71bed80709d7f83cee2c12fa9ea7e28fbd4b8d
Nicky de Vendt <nicky@sensimedia.nl>
2018-05-11 13:42:53 +0200

    initial databasing

---
+++ src/Entity/Affiliatedcompanys.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AffiliatedcompanysRepository")
+ */
+class Affiliatedcompanys
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $website;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datecreated;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?string
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(string $website): self
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+    public function getDatecreated(): ?\DateTimeInterface
+    {
+        return $this->datecreated;
+    }
+
+    public function setDatecreated(\DateTimeInterface $datecreated): self
+    {
+        $this->datecreated = $datecreated;
+
+        return $this;
+    }
+}
