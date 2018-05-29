9250b4297b0dddac2e489988d20da687e6144787
Nicky de Vendt <nicky@sensimedia.nl>
2018-05-29 11:52:07 +0200

    fixing resume page styling and get pdfs working

--- src/Controller/Resume.php
+++ src/Controller/Resume.php
@@ -12,9 +12,6 @@ class Resume extends Controller
     * @Route("/resume")
     */
     public function Resume() {
-        $_SESSION['admin'] = 0;
-        $_SESSION['userid'] = 0;
-
         return $this->render('resume.html.twig', array(
         'session' => $_SESSION,
         ));
@@ -23,9 +20,6 @@ class Resume extends Controller
     *   @Route("/resume/nlpdf")
     */
     public function getDutchPdf() {
-    // this needs async loading.....
-        $pdfdir = dirname(__DIR__, 2). '/generatedpdf/';
-
         if(isset($_POST['nlpdf'])) {
             $file = dirname(__DIR__,2) . '/generatedpdf/' . 'nl-nickydevendt.pdf';
 
@@ -54,14 +48,56 @@ class Resume extends Controller
     *   @Route("/resume/ukpdf")
     */
     public function getUkPdf() {
+        if(isset($_POST['ukpdf'])) {
+            $file = dirname(__DIR__,2) . '/generatedpdf/' . 'nickydevendt.pdf';
 
+            if (file_exists($file)) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment; filename="'.basename($file).'"');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($file));
+                readfile($file);
+                exit;
+            }
+            if(!file_exists($file)) {
+                $message =  '<div class="alert">
+                <span class="closebtn">&times;</span>
+                <strong>Error</strong> English pdf couldnt load in try again or get in contact with the admin
+                </div>';
+                echo $message;
+            }
+        }
     }
 
     /**
     *   @Route("/resume/gerpdf")
     */
     public function getGerPdf() {
+        if(isset($_POST['gerpdf'])) {
+            $file = dirname(__DIR__,2) . '/generatedpdf/' . 'ger-nickydevendt.pdf';
 
+            if (file_exists($file)) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment; filename="'.basename($file).'"');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($file));
+                readfile($file);
+                exit;
+            }
+            if(!file_exists($file)) {
+                $message =  '<div class="alert">
+                <span class="closebtn">&times;</span>
+                <strong>Error</strong> pdf could not be created try again or send the admin a email he can fix it!
+                </div>';
+                echo $message;
+            }
+        }
     }
 }
 
