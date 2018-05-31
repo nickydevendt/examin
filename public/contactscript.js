d350578e991a6a1105bcc3238930f871d2812999
Nicky de Vendt <nicky@sensimedia.nl>
2018-05-31 15:30:37 +0200

    fixing javascript across project

---
+++ public/contactscript.js
@@ -0,0 +1,19 @@
+function contactEmail() {
+    console.log('were reaching it!');
+    var email = $('#email').val();
+    var pattern = new RegExp("^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+    var result = pattern.test(email);
+    if(result == true) {
+        $('#loading').toggleClass('show');
+        $.post(
+            '/contact/sendmail',
+            { contactsend: true, email: contactform.email.value, name: contactform.name.value, telephone: contactform.phone.value, message: contactform.message.value },
+            function(output) {
+                $('#output').append(output);
+                $('.inputfield').val('');
+                $('#loading').toggleClass('show');
+            }
+        );
+    }
+}
+
