1af70c203f04430795451091d76cdd7051973880
Nicky de Vendt <nicky@sensimedia.nl>
2018-06-14 11:11:52 +0200

    Editing the pdf creator & file

--- src/Controller/Pdfcreator.php
+++ src/Controller/Pdfcreator.php
@@ -29,8 +29,9 @@ class Pdfcreator extends Controller
      */
     public function buildpdf() {
     // create new pdf document
+    $user = $this->getUser();
     $data = $_POST;
-    //var_dump($data);
+//    var_dump($user);
 
     $pdf = $this->container->get('tcpdf');
 
@@ -42,7 +43,7 @@ class Pdfcreator extends Controller
 
     $pdf->SetPrintHeader(false);
     $pdf->SetPrintFooter(false);
-    //$pdf->K_THAI_TOPCHARS(false);
+
     $pdf->setFontSubsetting(false);
 
     $pdf->AddPage();
@@ -50,8 +51,8 @@ class Pdfcreator extends Controller
     $pdf->writeHTML($html);
     $pdf->lastPage();
     ob_end_clean();
-    //ob_clean();
-    $filename = 'blarps';
+
+    $filename = $user['username'];
     $pdf->Output(dirname(__DIR__, 2) . '/generatedpdf/' . $filename, 'F');
     return new Response('we komen er wel');
     }
